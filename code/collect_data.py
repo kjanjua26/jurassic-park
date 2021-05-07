66fc207270fdefad21a0417e29b228843506f491
Kamran Janjua <kamranejaz98@gmail.com>
2021-05-08 00:51:03 +0300

    Collected dataset of dinos from a-z

--- code/collect_data.py
+++ code/collect_data.py
@@ -6,8 +6,12 @@ import requests, re
 from bs4 import BeautifulSoup
 
 base_url = "https://www.nhm.ac.uk/discover/dino-directory/name/{}/gallery.html"
-list_of_a_z = list(string.ascii_lowercase)
+alphabets = list(string.ascii_lowercase)
 header = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+
+data_file = open('data.csv', 'w+')
+data_file.write('name' + ',' + 'diet' + ',' + 'period' + ',' + 'lived_in' + ',' + 'type' + ',' + 'length'
+                + ',' + 'taxonomy' + ',' + 'named_by' + ',' + 'species' + '\n')
 
 class JurassicPark:
     def __init__(self, letter):
@@ -26,25 +30,73 @@ class JurassicPark:
         for ix, dino_url in enumerate(dino_urls):
             dino_link = str(dino_url).split('href')[-1].split('">\n')[0].replace('="', '')
             lst_of_dinos.append(dino_link)
-
         return lst_of_dinos
     
     def get_dino_data_per_url(self):
-        dino_data = {}
-
         lst_of_dinos = self.get_dino_urls()
+
+        dinos = []
+
         for ix, dino in enumerate(lst_of_dinos):
+            dino_data = {}
+            
+            diet_data = []
+            taxonomy_data = []
+
             soup = self.read_a_page(dino)
-            info = soup.find_all('div', attrs={'class':'row'})
+
             type_length = soup.find_all('dl', attrs={'class':'dinosaur--description dinosaur--list'})
             diet = soup.find_all('div', attrs={'class':'dinosaur--info-container small-12 medium-12 large-7 columns'})
             taxonomy = soup.find_all('div', attrs={'class':'dinosaur--taxonomy-container small-12 medium-12 large-12 columns'})
+            name = dino.split('/')[-1].split('.')[0]
+            
+            type_ = str(type_length).split('<dd>')[-2].split('.html">')[-1].split('</a>')[0].strip()
+            length = str(type_length).split('<dd>')[-1].split('</dd>')[0].strip()
 
-            print(ix, dino)
-            print(type_length)
-            print(diet)
-            print(taxonomy)
-            exit()
+            for vals in str(diet).split('<dd>'):
+                lines = vals.split('">')[-1].split('</a>')[0]
+                period = vals.split('">')[-1].split(', ')[-1].split('</dd>')[0]
+                if not bool(BeautifulSoup(lines, "html.parser").find()):
+                    if lines not in period:
+                        lines = lines + ' ' + period    
+                    diet_data.append(lines)
 
-jr = JurassicPark("a")
-jr.get_dino_data_per_url()
+            for vals in str(taxonomy).split('<dd>'):
+                lines = vals.split('">')[-1].split('</a>')[0].split('</dd>')[0]
+                if not bool(BeautifulSoup(lines, "html.parser").find()):
+                    taxonomy_data.append(lines)
+           
+            dino_data["name"] = name
+            dino_data["diet"] = diet_data[0]
+            dino_data["period"] = diet_data[1]
+            dino_data["lived_in"] = diet_data[2]
+            dino_data["type"] = type_
+            dino_data["length"] = length
+            dino_data["taxonomy"] = taxonomy_data[0]
+            dino_data["named_by"] = taxonomy_data[1]
+            dino_data["species"] = taxonomy_data[2]
+            dinos.append(dino_data)
+
+        return dinos
+
+for ix, alphabet in enumerate(alphabets):
+    jr = JurassicPark(alphabet)
+    dinos = jr.get_dino_data_per_url()
+    
+    for dino in dinos:
+        name = dino["name"].replace(',', '')
+        diet = dino["diet"].replace(',', '')
+        period = dino["period"].replace(',', '')
+        lived_in = dino["lived_in"].replace(',', '')
+        typ = dino["type"].replace(',', '')
+        length = dino["length"].replace(',', '')
+        taxonomy = dino["taxonomy"].replace(',', '')
+        named_by = dino["named_by"].replace(',', '')
+        species = dino["species"].replace(',', '')
+        
+        data_file.write(name + ',' + diet + ',' + period + ','
+                + lived_in + ',' + typ + ',' + length + ',' + 
+                taxonomy + ',' + named_by + ',' + species)
+        data_file.write('\n')
+    
+    exit()
